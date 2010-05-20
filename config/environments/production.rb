4b923448f9fb7c622fe146d6fc3dedaa6aad937c
Ryan R. Smith <this.ryansmith@gmail.com>
2010-05-20 17:23:49 -0500

    updated session domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,4 +26,4 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
-config.action_controller.session[:domain] = ".requestdaysoff.com"
+config.action_controller.session[:domain] = ".timeoffhq.com"
