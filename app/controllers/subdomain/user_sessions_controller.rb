231cf963b4331ca0750a914733576a7732cb3546
Ryan R. Smith <this.ryansmith@gmail.com>
2010-05-21 09:39:58 -0500

    updated the sign in screen

--- app/controllers/subdomain/user_sessions_controller.rb
+++ app/controllers/subdomain/user_sessions_controller.rb
@@ -1,4 +1,6 @@
 class Subdomain::UserSessionsController < ApplicationController
+  layout 'user_sessions'
+
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   before_filter :require_account
