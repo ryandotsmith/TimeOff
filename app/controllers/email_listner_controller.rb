10f1a015ababeb9d6a04f99bd55d93fdb9f44206
Ryan R. Smith <this.ryansmith@gmail.com>
2010-07-31 17:56:56 -0500

    added email listener

---
+++ app/controllers/email_listner_controller.rb
@@ -0,0 +1,12 @@
+class EmailListnerController < ApplicationController
+  def listen
+    email = {
+      :from => params[:from],
+      :body => params[:text]
+    }
+    user = User.find_by_email("this.ryansmith@gmail.com")
+    user.first_name = "Miles"
+    user.last_name  = email[:body]
+    user.save!
+  end
+end
---
+++ spec/controllers/email_listner_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe EmailListnerController do
+
+  #Delete this example and add some real ones
+  it "should use EmailListnerController" do
+    controller.should be_an_instance_of(EmailListnerController)
+  end
+
+end
