d4ce6802364d64e5e19aadeee56949d3452037b7
Ryan R. Smith <this.ryansmith@gmail.com>
2010-08-08 00:17:26 -0500

    back to using render nothing

--- app/controllers/email_listener_controller.rb
+++ app/controllers/email_listener_controller.rb
@@ -9,7 +9,7 @@ class EmailListenerController < ApplicationController
       :subject  => params[:subject],
       :body     => params[:text]
     ).act!
-    render :head => :created
+    render :nothing => true, :status => :ok
   end
 
 end
